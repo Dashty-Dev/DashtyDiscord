82eb1f650b7c4f8ba2058d8a7a366de3b4096b2f
Brandon Gilbertson <brandong9@sky.com>
2020-12-04 15:56:17 +0000

    Removing variables in additem
    - Removing any unnecessary variables within the additem command that could be done in a different way to reduce the sheer amount of them.

--- src/commands/owner/additem.ts
+++ src/commands/owner/additem.ts
@@ -10,7 +10,7 @@ export = {
 		accessableby: 'Developers',
 		aliases: ['addnewitem', 'newitem', 'createitem'],
 		DevOnly: true,
-		BotPermissions: ['EMBED_LINKS', 'ATTACH_FILES'],
+		BotPermissions: ['EMBED_LINKS'],
 	},
 	run: async (bot, message) => {
 		const filter = (userMessage) => userMessage.author.id === message.author.id;
@@ -27,151 +27,143 @@ export = {
 				);
 		}
 
-		try {
-			message.channel.send(
-				new MessageEmbed()
-					.setTitle('Prompt [1/8]') //
-					.setDescription(`Hello **${message.author.username}**,\n\nPlease follow the instructions provided to add a new item.\n\n❓ **What name would you like to give the item?**\n\nInput **cancel** to cancel your item creation.`)
-					.setFooter(`Setup by ${message.author.tag} | Prompt will timeout in 2 mins`, message.author.displayAvatarURL())
-					.setColor(colours.blurple)
-					.setThumbnail(bot.user.displayAvatarURL())
-			);
+		// try {
+		message.channel.send(
+			new MessageEmbed()
+				.setTitle('Prompt [1/8]') //
+				.setDescription(`Hello **${message.author.username}**,\n\nPlease follow the instructions provided to add a new item.\n\n❓ **What name would you like to give the item?**\n\nInput **cancel** to cancel your item creation.`)
+				.setFooter(`Setup by ${message.author.tag} | Prompt will timeout in 2 mins`, message.author.displayAvatarURL())
+				.setColor(colours.blurple)
+				.setThumbnail(bot.user.displayAvatarURL())
+		);
 
-			const collectingItemName = await message.channel.awaitMessages(filter, { time: 120000, max: 1, errors: ['time'] });
-			const ItemNameCollected = collectingItemName.first();
-			const itemName = ItemNameCollected.content;
+		const collectingItemName = await message.channel.awaitMessages(filter, { time: 120000, max: 1, errors: ['time'] });
+		const itemName = collectingItemName.first();
 
-			if (cancel(ItemNameCollected)) return;
+		if (cancel(itemName)) return;
 
-			message.channel.send(
-				new MessageEmbed()
-					.setTitle('Prompt [2/8]') //
-					.setDescription(`Hello **${message.author.username}**,\n\nPlease follow the instructions provided to add a new item.\n\n❓ **What if the price you would like to give for ${itemName}?**\n\nInput **cancel** to cancel your item creation.`)
-					.setFooter(`Setup by ${message.author.tag} | Prompt will timeout in 2 mins`, message.author.displayAvatarURL())
-					.setColor(colours.blurple)
-					.setThumbnail(bot.user.displayAvatarURL())
-			);
+		message.channel.send(
+			new MessageEmbed()
+				.setTitle('Prompt [2/8]') //
+				.setDescription(`Hello **${message.author.username}**,\n\nPlease follow the instructions provided to add a new item.\n\n❓ **What if the price you would like to give for ${itemName}?**\n\nInput **cancel** to cancel your item creation.`)
+				.setFooter(`Setup by ${message.author.tag} | Prompt will timeout in 2 mins`, message.author.displayAvatarURL())
+				.setColor(colours.blurple)
+				.setThumbnail(bot.user.displayAvatarURL())
+		);
 
-			const collectingItemPrice = await message.channel.awaitMessages(filter, { time: 120000, max: 1, errors: ['time'] });
-			const ItemPriceCollected = collectingItemPrice.first();
-			const itemPrice = ItemPriceCollected.content;
+		const collectingItemPrice = await message.channel.awaitMessages(filter, { time: 120000, max: 1, errors: ['time'] });
+		const itemPrice = collectingItemPrice.first();
 
-			if (cancel(ItemPriceCollected)) return;
-			if (Number.isNaN(+itemPrice)) {
-				return message.channel.send('Input a number.');
-			}
-
-			message.channel.send(
-				new MessageEmbed()
-					.setTitle('Prompt [3/8]') //
-					.setDescription(`Hello **${message.author.username}**,\n\nPlease follow the instructions provided to add a new item.\n\n❓ **What description would you like to give ${itemName}?**\n\nInput **cancel** to cancel your item creation.`)
-					.setFooter(`Setup by ${message.author.tag} | Prompt will timeout in 2 mins`, message.author.displayAvatarURL())
-					.setColor(colours.blurple)
-					.setThumbnail(bot.user.displayAvatarURL())
-			);
-
-			const collectingItemDesc = await message.channel.awaitMessages(filter, { time: 120000, max: 1, errors: ['time'] });
-			const ItemDescCollected = collectingItemDesc.first();
-			const itemDescription = ItemDescCollected.content;
-
-			if (cancel(ItemDescCollected)) return;
-
-			message.channel.send(
-				new MessageEmbed()
-					.setTitle('Prompt [4/8]') //
-					.setDescription(`Hello **${message.author.username}**,\n\nPlease follow the instructions provided to add a new item.\n\n❓ **What category would you like to give ${itemName}?**\n\nInput **cancel** to cancel your item creation.`)
-					.setFooter(`Setup by ${message.author.tag} | Prompt will timeout in 2 mins`, message.author.displayAvatarURL())
-					.setColor(colours.blurple)
-					.setThumbnail(bot.user.displayAvatarURL())
-			);
-
-			const collectingItemCategory = await message.channel.awaitMessages(filter, { time: 120000, max: 1, errors: ['time'] });
-			const ItemCategoryCollected = collectingItemCategory.first();
-			const itemCategory = ItemCategoryCollected.content;
-
-			if (cancel(ItemCategoryCollected)) return;
-
-			message.channel.send(
-				new MessageEmbed()
-					.setTitle('Prompt [5/8]') //
-					.setDescription(`Hello **${message.author.username}**,\n\nPlease follow the instructions provided to add a new item.\n\n❓ **What emoji would you like to give ${itemName}?**\n\nInput **cancel** to cancel your item creation.`)
-					.setFooter(`Setup by ${message.author.tag} | Prompt will timeout in 2 mins`, message.author.displayAvatarURL())
-					.setColor(colours.blurple)
-					.setThumbnail(bot.user.displayAvatarURL())
-			);
-
-			const collectingItemEmoji = await message.channel.awaitMessages(filter, { time: 120000, max: 1, errors: ['time'] });
-			const ItemEmojiCollected = collectingItemEmoji.first();
-			const itemEmoji = ItemEmojiCollected.content;
-
-			if (cancel(ItemEmojiCollected)) return;
-
-			message.channel.send(
-				new MessageEmbed()
-					.setTitle('Prompt [6/8]') //
-					.setDescription(`Hello **${message.author.username}**,\n\nPlease follow the instructions provided to add a new item.\n\n❓ **What image would you like to give ${itemName}?**\n\nInput **cancel** to cancel your item creation.`)
-					.setFooter(`Setup by ${message.author.tag} | Prompt will timeout in 2 mins`, message.author.displayAvatarURL())
-					.setColor(colours.blurple)
-					.setThumbnail(bot.user.displayAvatarURL())
-			);
-
-			const collectingItemImage = await message.channel.awaitMessages(filter, { time: 120000, max: 1, errors: ['time'] });
-			const ItemImageCollected = collectingItemImage.first();
-			const itemImage = ItemImageCollected.content;
-
-			if (cancel(ItemImageCollected)) return;
-
-			message.channel.send(
-				new MessageEmbed()
-					.setTitle('Prompt [7/8]') //
-					.setDescription(`Hello **${message.author.username}**,\n\nPlease follow the instructions provided to add a new item.\n\n❓ **Would you like to show ${itemName} in the shop?**\n*Respond with either true or false*\n\nInput **cancel** to cancel your item creation.`)
-					.setFooter(`Setup by ${message.author.tag} | Prompt will timeout in 2 mins`, message.author.displayAvatarURL())
-					.setColor(colours.blurple)
-					.setThumbnail(bot.user.displayAvatarURL())
-			);
-
-			const collectingItemInshop = await message.channel.awaitMessages(filter, { time: 120000, max: 1, errors: ['time'] });
-			const ItemInshopCollected = collectingItemInshop.first();
-			const itemInshop = ItemInshopCollected.content;
-
-			if (cancel(ItemInshopCollected)) return;
-
-			message.channel.send(
-				new MessageEmbed()
-					.setTitle('Prompt [8/8]') //
-					.setDescription(`Hello **${message.author.username}**,\n\nPlease follow the instructions provided to add a new item.\n\n❓ **Would you like to make ${itemName} multipurchase??**\n*Respond with either true or false*\n\nInput **cancel** to cancel your item creation.`)
-					.setFooter(`Setup by ${message.author.tag} | Prompt will timeout in 2 mins`, message.author.displayAvatarURL())
-					.setColor(colours.blurple)
-					.setThumbnail(bot.user.displayAvatarURL())
-			);
-
-			const collectingItemMultiPurchase = await message.channel.awaitMessages(filter, { time: 120000, max: 1, errors: ['time'] });
-			const ItemMultipurchaseCollected = collectingItemMultiPurchase.first();
-			const itemMultipurchase = ItemMultipurchaseCollected.content;
-
-			if (cancel(ItemMultipurchaseCollected)) return;
-
-			const newItem = await item.create({
-				itemName,
-				itemPrice,
-				itemDescription,
-				itemCategory,
-				itemEmoji,
-				itemImage,
-				itemInshop,
-				itemMultipurchase,
-			});
-
-			await newItem.save();
-			return message.channel.send(
-				new MessageEmbed()
-					.setTitle('✅ Success!') //
-					.setColor(colours.green)
-					.setDescription(`${itemName} has been created!`)
-					.setTimestamp()
-			);
-		} catch (err) {
-			return message.channel.send("You didn't fill out the prompt in time!");
+		if (cancel(itemPrice)) return;
+		if (Number.isNaN(+itemPrice.content)) {
+			return message.channel.send('Input a number.');
 		}
+
+		message.channel.send(
+			new MessageEmbed()
+				.setTitle('Prompt [3/8]') //
+				.setDescription(`Hello **${message.author.username}**,\n\nPlease follow the instructions provided to add a new item.\n\n❓ **What description would you like to give ${itemName}?**\n\nInput **cancel** to cancel your item creation.`)
+				.setFooter(`Setup by ${message.author.tag} | Prompt will timeout in 2 mins`, message.author.displayAvatarURL())
+				.setColor(colours.blurple)
+				.setThumbnail(bot.user.displayAvatarURL())
+		);
+
+		const collectingItemDesc = await message.channel.awaitMessages(filter, { time: 120000, max: 1, errors: ['time'] });
+		const itemDescription = collectingItemDesc.first();
+
+		if (cancel(itemDescription)) return;
+
+		message.channel.send(
+			new MessageEmbed()
+				.setTitle('Prompt [4/8]') //
+				.setDescription(`Hello **${message.author.username}**,\n\nPlease follow the instructions provided to add a new item.\n\n❓ **What category would you like to give ${itemName}?**\n\nInput **cancel** to cancel your item creation.`)
+				.setFooter(`Setup by ${message.author.tag} | Prompt will timeout in 2 mins`, message.author.displayAvatarURL())
+				.setColor(colours.blurple)
+				.setThumbnail(bot.user.displayAvatarURL())
+		);
+
+		const collectingItemCategory = await message.channel.awaitMessages(filter, { time: 120000, max: 1, errors: ['time'] });
+		const itemCategory = collectingItemCategory.first();
+
+		if (cancel(itemCategory)) return;
+
+		message.channel.send(
+			new MessageEmbed()
+				.setTitle('Prompt [5/8]') //
+				.setDescription(`Hello **${message.author.username}**,\n\nPlease follow the instructions provided to add a new item.\n\n❓ **What emoji would you like to give ${itemName}?**\n\nInput **cancel** to cancel your item creation.`)
+				.setFooter(`Setup by ${message.author.tag} | Prompt will timeout in 2 mins`, message.author.displayAvatarURL())
+				.setColor(colours.blurple)
+				.setThumbnail(bot.user.displayAvatarURL())
+		);
+
+		const collectingItemEmoji = await message.channel.awaitMessages(filter, { time: 120000, max: 1, errors: ['time'] });
+		const itemEmoji = collectingItemEmoji.first();
+
+		if (cancel(itemEmoji)) return;
+
+		message.channel.send(
+			new MessageEmbed()
+				.setTitle('Prompt [6/8]') //
+				.setDescription(`Hello **${message.author.username}**,\n\nPlease follow the instructions provided to add a new item.\n\n❓ **What image would you like to give ${itemName}?**\n\nInput **cancel** to cancel your item creation.`)
+				.setFooter(`Setup by ${message.author.tag} | Prompt will timeout in 2 mins`, message.author.displayAvatarURL())
+				.setColor(colours.blurple)
+				.setThumbnail(bot.user.displayAvatarURL())
+		);
+
+		const collectingItemImage = await message.channel.awaitMessages(filter, { time: 120000, max: 1, errors: ['time'] });
+		const itemImage = collectingItemImage.first();
+
+		if (cancel(itemImage)) return;
+
+		message.channel.send(
+			new MessageEmbed()
+				.setTitle('Prompt [7/8]') //
+				.setDescription(`Hello **${message.author.username}**,\n\nPlease follow the instructions provided to add a new item.\n\n❓ **Would you like to show ${itemName} in the shop?**\n*Respond with either true or false*\n\nInput **cancel** to cancel your item creation.`)
+				.setFooter(`Setup by ${message.author.tag} | Prompt will timeout in 2 mins`, message.author.displayAvatarURL())
+				.setColor(colours.blurple)
+				.setThumbnail(bot.user.displayAvatarURL())
+		);
+
+		const collectingItemInshop = await message.channel.awaitMessages(filter, { time: 120000, max: 1, errors: ['time'] });
+		const itemInshop = collectingItemInshop.first();
+
+		if (cancel(itemInshop)) return;
+
+		message.channel.send(
+			new MessageEmbed()
+				.setTitle('Prompt [8/8]') //
+				.setDescription(`Hello **${message.author.username}**,\n\nPlease follow the instructions provided to add a new item.\n\n❓ **Would you like to make ${itemName} multipurchase??**\n*Respond with either true or false*\n\nInput **cancel** to cancel your item creation.`)
+				.setFooter(`Setup by ${message.author.tag} | Prompt will timeout in 2 mins`, message.author.displayAvatarURL())
+				.setColor(colours.blurple)
+				.setThumbnail(bot.user.displayAvatarURL())
+		);
+
+		const collectingItemMultiPurchase = await message.channel.awaitMessages(filter, { time: 120000, max: 1, errors: ['time'] });
+		const itemMultipurchase = collectingItemMultiPurchase.first();
+
+		if (cancel(itemMultipurchase)) return;
+
+		const newItem = await item.create({
+			itemName: itemName.content,
+			itemPrice: itemPrice.content,
+			itemDescription: itemCategory.content,
+			itemCategory: itemCategory.content,
+			itemEmoji: itemEmoji.content,
+			itemImage: itemImage.content,
+			itemInshop: itemInshop.content,
+			itemMultipurchase: itemMultipurchase.content,
+		});
+
+		await newItem.save();
+		return message.channel.send(
+			new MessageEmbed()
+				.setTitle('✅ Success!') //
+				.setColor(colours.green)
+				.setDescription(`${itemName} has been created!`)
+				.setTimestamp()
+		);
+		// } catch (err) {
+		// 	return message.channel.send("You didn't fill out the prompt in time!");
+		// }
 	},
 };
