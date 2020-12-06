f878805376ec586294851c0d2a1804f88b9a84c3
Brandon Gilbertson <brandong9@sky.com>
2020-12-06 15:58:10 +0000

    Loading updates
    - Throwing new errors if file doesn't exist or if there is no config instead of an error that may confuse people.
    - Adding in more typescript types

--- src/handlers/loadCommands.ts
+++ src/handlers/loadCommands.ts
@@ -2,12 +2,16 @@ import { readdirSync } from 'fs';
 
 export = (bot: any) => {
 	const load = (directories: string) => {
-		const commands = readdirSync(`${__dirname}../../commands/${directories}/`).filter((directoryFile) => directoryFile.endsWith('.js'));
-		commands.forEach((commandFile) => {
+		const commands: string[] = readdirSync(`${__dirname}../../commands/${directories}/`).filter((directoryFile: string) => directoryFile.endsWith('.js'));
+		if (commands.length <= 0) throw new Error(`The folder ${directories} could not be loaded. Please ensure a file is added in it to be loaded.`);
+
+		commands.forEach((commandFile: string) => {
 			const command = require(`${__dirname}../../commands/${directories}/${commandFile}`);
+
+			if (!command.config) throw new Error(`The command ${commandFile} was not loaded. Command requires a config for it to be used.`);
 			bot.commands.set(command.config.name, command);
-			if (command.config.aliases) command.config.aliases.forEach((alias: any) => bot.aliases.set(alias, command.config.name));
+			if (command.config.aliases) command.config.aliases.forEach((alias: string) => bot.aliases.set(alias, command.config.name));
 		});
 	};
-	['misc', 'moderation', 'information', 'owner', 'configuration'].forEach((folder) => load(folder));
+	['misc', 'moderation', 'information', 'owner', 'configuration', 'economy'].forEach((folder) => load(folder));
 };
--- src/handlers/loadEvents.ts
+++ src/handlers/loadEvents.ts
@@ -1,11 +1,15 @@
+import { Client } from 'discord.js';
 import { readdirSync } from 'fs';
 
-export = (bot: any) => {
+export = (bot: Client) => {
 	const load = (directories: string) => {
-		const events = readdirSync(`${__dirname}../../events/${directories}/`).filter((directoryFile) => directoryFile.endsWith('.js'));
-		events.forEach((eventFile) => {
+		const events: string[] = readdirSync(`${__dirname}../../events/${directories}/`).filter((directoryFile: string) => directoryFile.endsWith('.js'));
+		if (events.length <= 0) throw new Error(`The folder ${directories} could not be loaded. Please ensure a file is added in it to be loaded.`);
+
+		events.forEach((eventFile: string) => {
 			const event = require(`${__dirname}../../events/${directories}/${eventFile}`);
-			const eventName = eventFile.split('.')[0];
+			const eventName: string = eventFile.split('.')[0];
+
 			bot.on(eventName, event.bind(null, bot));
 		});
 	};
