52589c3c4de1b7d572eb0d748827c1cd52b77d20
Brandon <brandong9@sky.com>
2021-01-02 13:16:53 +0000

    temp test command

---
+++ src/commands/moderation/testcommand.ts
@@ -0,0 +1,15 @@
+import { Client, Message } from 'discord.js';
+
+export = {
+  config: {
+    name: 'testcommand',
+    description: "Displays a list of all Dashty's commands.",
+    usage: '.help',
+    accessableby: 'Public',
+    aliases: ['dashty', 'commands', 'cmds'],
+    ServerOnly: false,
+  },
+  run: async (bot: Client, message: Message, args: string[]) => {
+    message.channel.send('you are a noob now be quiet');
+  },
+};
