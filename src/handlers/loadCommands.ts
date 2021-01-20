50c58aef15431f87c3a7d82c5b6c3852a05a52db
Brandon <brandong9@sky.com>
2021-01-20 15:04:50 +0000

    Changing handler paths

--- src/handlers/loadCommands.ts
+++ src/handlers/loadCommands.ts
@@ -2,11 +2,11 @@ import { readdirSync } from 'fs';
 
 export = (bot: any) => {
   const load = (directories: string) => {
-    const commands: string[] = readdirSync(`${__dirname}../../commands/${directories}/`).filter((directoryFile: string) => directoryFile.endsWith('.js'));
+    const commands: string[] = readdirSync(`${__dirname}/../commands/${directories}/`).filter((directoryFile: string) => directoryFile.endsWith('.js'));
     if (commands.length <= 0) throw new Error(`The folder ${directories} could not be loaded. Please ensure a file is added in it to be loaded.`);
 
     commands.forEach((commandFile: string) => {
-      const command = require(`${__dirname}../../commands/${directories}/${commandFile}`);
+      const command = require(`${__dirname}/../commands/${directories}/${commandFile}`);
 
       if (!command.config) throw new Error(`The command ${commandFile} was not loaded. Command requires a config for it to be used.`);
       bot.commands.set(command.config.name, command);
--- src/handlers/loadEvents.ts
+++ src/handlers/loadEvents.ts
@@ -3,11 +3,11 @@ import { readdirSync } from 'fs';
 
 export = (bot: Client) => {
   const load = (directories: string) => {
-    const events: string[] = readdirSync(`${__dirname}../../events/${directories}/`).filter((directoryFile: string) => directoryFile.endsWith('.js'));
+    const events: string[] = readdirSync(`${__dirname}/../events/${directories}/`).filter((directoryFile: string) => directoryFile.endsWith('.js'));
     if (events.length <= 0) throw new Error(`The folder ${directories} could not be loaded. Please ensure a file is added in it to be loaded.`);
 
     events.forEach((eventFile: string) => {
-      const event = require(`${__dirname}../../events/${directories}/${eventFile}`);
+      const event = require(`${__dirname}/../events/${directories}/${eventFile}`);
       const eventName: string = eventFile.split('.')[0];
 
       bot.on(eventName, event.bind(null, bot));
